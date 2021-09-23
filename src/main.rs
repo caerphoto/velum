4c292ea284892b1ed1c6d640feda2dc1a9bb10cc
Andy Farrell <git@andyf.me>
2021-09-24 00:30:00 +0100

    Improve startup error checking

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,25 @@ use std::env;
 use std::path::PathBuf;
 use std::fs;
 
+fn path_if_valid(base: &str, dir: &str) -> PathBuf {
+    let path: PathBuf = [&base, dir].iter().collect();
+    match fs::metadata(&path) {
+        Ok(metadata) => {
+            if metadata.is_dir() {
+                metadata
+            } else {
+                println!("Error: {} is not a directory", path.display());
+                process::exit(1);
+            }
+        },
+        Err(_) => {
+            println!("Error: {} doesn't exist or is not accessible", path.display());
+            process::exit(1);
+        }
+    };
+    path
+}
+
 fn main() {
     let base_key = "VELUM_BASE";
     let base_path = match env::var(base_key) {
@@ -12,23 +31,10 @@ fn main() {
             process::exit(1);
         }
     };
-    let templ_path: PathBuf = [&base_path, "templates"].iter().collect();
 
-    match fs::metadata(&templ_path) {
-        Ok(m) => {
-            if m.is_dir() {
-                m
-            } else {
-                println!("Error: {} is not a directory", templ_path.display());
-                process::exit(1);
-            }
-        },
-        Err(_) => {
-            println!("Error: {} doesn't exist or is not accessible", templ_path.display());
-            process::exit(1);
-        }
-    };
-
+    let templ_path = path_if_valid(&base_path, "templates");
+    let article_path = path_if_valid(&base_path, "articles");
 
     println!("Looking for templates in {}", templ_path.display());
+    println!("Looking for articles in {}", article_path.display());
 }
