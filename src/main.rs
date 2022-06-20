76696ee896af58441cbc215614afbb44ca8d50f7
Andy Farrell <git@andyf.me>
2022-06-20 15:49:39 +0100

    Refactor; show next/prev on article page
    Some stuff that was in Article is now a separate, lightweight
    ArticleView, which is used for rendering in a template and when
    searching through articles to find the previous/next one.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::path::{Path, PathBuf};
 use std::io::{self, ErrorKind};
 use std::{fs, time, cmp};
 use std::collections::BTreeMap;
-use serde::ser::{Serialize, Serializer, SerializeStruct};
+use serde::Serialize;
 use serde_json::json;
 use warp::Filter;
 use handlebars::{Handlebars, handlebars_helper};
@@ -25,10 +25,16 @@ const BLOG_TITLE: &str = "Velum Test Blog";
 const DEFAULT_TITLE: &str = "<no title>";
 const UNIX_EPOCH: time::SystemTime = time::SystemTime::UNIX_EPOCH;
 
+fn article_keys(con: &mut redis::Connection) -> Result<Vec<String>, redis::RedisError> {
+    let keys: Vec<String> = con.keys(String::from(BASE_KEY) + "*")?;
+    Ok(keys)
+}
+
 // Struct for creating and managing article data
+// TODO: move this to a lib
 struct Article {
     content: String,
-    timestamp_millis: i64,
+    timestamp: i64,
 }
 
 impl Article {
@@ -39,26 +45,11 @@ impl Article {
         if let Ok(s) = created.duration_since(UNIX_EPOCH) {
             Ok(Self {
                 content,
-                timestamp_millis: s.as_millis() as i64
+                timestamp: s.as_millis() as i64
             })
         } else {
             Err(io::Error::new(ErrorKind::Other, "failed to read file"))
         }
-    }
-
-    fn from_redis(a: &BTreeMap<String, String>) -> Self {
-        let timestamp = a.get("timestamp").unwrap();
-        Self {
-            content: a.get("content").unwrap().to_string(),
-            timestamp_millis: timestamp.parse::<i64>().unwrap_or(0),
-        }
-    }
-
-    fn parsed_content(&self) -> String {
-        let mut parsed_article = String::new();
-        let parser = cmark::Parser::new(&self.content);
-        cmark::html::push_html(&mut parsed_article, parser);
-        parsed_article
     }
 
     fn title(&self) -> Option<String> {
@@ -98,22 +89,95 @@ impl Article {
     // For passing to Redis via hset_multiple
     fn to_kv_list(&self) -> Box<[(String, String)]> {
         Box::new([
+            ("title".to_string(), self.title().unwrap_or(DEFAULT_TITLE.to_string())),
             ("content".to_string(), self.content.clone()),
-            ("timestamp".to_string(), self.timestamp_millis.to_string()),
+            ("route".to_string(), self.route().unwrap_or("/".to_string())),
+            ("timestamp".to_string(), self.timestamp.to_string()),
         ])
     }
 }
 
-impl Serialize for Article {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where S: Serializer {
-        let mut state = serializer.serialize_struct("Article", 4)?;
-        let title = &self.title().unwrap_or(String::from(DEFAULT_TITLE));
-        state.serialize_field("title", title)?;
-        state.serialize_field("content", &self.parsed_content())?;
-        state.serialize_field("route", &self.route().unwrap_or("/".to_string()))?;
-        state.serialize_field("timestamp", &self.timestamp_millis)?;
-        state.end()
+#[derive(Serialize, Clone)]
+struct ArticleView {
+    title: String,
+    content: String,
+    route: String,
+    timestamp: i64,
+}
+
+impl ArticleView {
+    fn parse_content(content: &str) -> String {
+        let mut parsed_article = String::new();
+        let no_title: String = content
+            .lines()
+            .skip(1)
+            .collect::<Vec<&str>>()
+            .join("\n");
+        let parser = cmark::Parser::new(&no_title);
+        cmark::html::push_html(&mut parsed_article, parser);
+        parsed_article
+    }
+
+
+    fn from_redis(a: &BTreeMap<String, String>, parse_content: bool) -> Self {
+        let timestamp = a.get("timestamp").unwrap();
+        let content = if parse_content {
+            ArticleView::parse_content(a.get("content").unwrap())
+        } else {
+            String::from("")
+        };
+
+        Self {
+            title: a.get("title").unwrap().to_string(),
+            content,
+            route: a.get("route").unwrap().to_string(),
+            timestamp: timestamp.parse::<i64>().unwrap_or(0),
+        }
+    }
+
+    fn surrounding(&self, con: &mut redis::Connection) -> (Option<Self>, Option<Self>) {
+        let mut articles: Vec<ArticleView> = Vec::new();
+
+        let keys = match article_keys(con) {
+            Ok(k) => k,
+            Err(_) => return (None, None)
+        };
+        for key in keys {
+            if let Ok(result) = con.hgetall(key) {
+                articles.push(ArticleView::from_redis(&result, false))
+            }
+        };
+        articles.sort_by_key(|a| -a.timestamp);
+        for (index, article) in articles.iter().enumerate() {
+            if article.timestamp == self.timestamp {
+                let prev = if index > 0 {
+                    match articles.get(index - 1) {
+                        Some(a) => Some(a.clone()),
+                        None => None
+                    }
+                } else { None };
+                let next = if index < articles.len() - 1 {
+                    match articles.get(index + 1) {
+                        Some(a) => Some(a.clone()),
+                        None => None
+                    }
+                } else { None };
+                return (prev, next);
+            }
+        }
+
+        (None, None)
+    }
+}
+
+impl From<Article> for ArticleView {
+    fn from(a: Article) -> Self {
+        Self {
+            title: a.title().unwrap_or(DEFAULT_TITLE.to_string()),
+            content: a.content.clone(),
+            route: a.route().unwrap_or("/".to_string()),
+            timestamp: a.timestamp,
+        }
     }
 }
 
@@ -121,10 +185,6 @@ impl Serialize for Article {
 handlebars_helper!(date_from_timestamp: |ts: i64| {
     let dt = Utc.timestamp_millis(ts);
     dt.to_rfc2822()
-});
-
-handlebars_helper!(page_display: |current: usize, max: usize| {
-    format!("Page {} of {}", current + 1, max + 1)
 });
 
 #[derive(Debug)]
@@ -151,18 +211,18 @@ fn gather_fs_articles() -> Result<Vec<Article>, io::Error> {
     Ok(articles)
 }
 
-fn gather_redis_articles() -> Result<Vec<Article>, redis::RedisError> {
+fn gather_redis_article_views() -> Result<Vec<ArticleView>, redis::RedisError> {
     let client = redis::Client::open(REDIS_HOST)?;
     let mut con = client.get_connection()?;
-    let mut articles: Vec<Article> = Vec::new();
-    let keys: Vec<String> = con.keys(String::from(BASE_KEY) + "*")?;
+    let mut articles: Vec<ArticleView> = Vec::new();
+    let keys = article_keys(&mut con)?;
     for key in keys {
         if let Ok(result) = con.hgetall(key) {
-            articles.push(Article::from_redis(&result))
+            articles.push(ArticleView::from_redis(&result, true))
         }
     }
 
-    articles.sort_by_key(|a| -a.timestamp_millis);
+    articles.sort_by_key(|a| -a.timestamp);
 
     Ok(articles)
 }
@@ -195,20 +255,20 @@ fn rebuild_redis_data() -> redis::RedisResult<()> {
 }
 
 fn render_index_page(page: usize, hbs: &Handlebars<'_>) -> String {
-    if let Ok(articles) = gather_redis_articles() {
-        let pages: Vec<&[Article]> = articles.chunks(PAGE_SIZE).collect();
-        let max_page = pages.len().checked_sub(1).unwrap_or(0);
-        let constrained_page = cmp::min(max_page, page);
+    if let Ok(articles) = gather_redis_article_views() {
+        let pages: Vec<&[ArticleView]> = articles.chunks(PAGE_SIZE).collect();
+        let max_page = pages.len();
+        let chunk_index = cmp::min(max_page, page.checked_sub(1).unwrap_or(0));
 
         match hbs.render(
             "main",
             &json!({
                 "title": BLOG_TITLE,
-                "prev_page": page.checked_sub(1).unwrap_or(0),
+                "prev_page": if page > 1 { page - 1 } else { 0 },
                 "current_page": page,
-                "max_page": max_page,
                 "next_page": if page < max_page { page + 1 } else { 0 },
-                "articles": &pages[constrained_page]
+                "max_page": max_page,
+                "articles": &pages[chunk_index]
             })
         ) {
             Ok(rendered_page) => rendered_page,
@@ -229,11 +289,17 @@ fn render_article(slug: String, hbs: &Handlebars<'_>) -> impl warp::Reply {
     let mut con = client.get_connection().unwrap();
 
     if let Ok(result) = con.hgetall(String::from(BASE_KEY) + &slug) {
-        let article = Article::from_redis(&result);
+        let article = ArticleView::from_redis(&result, true);
+        let (prev, next) = article.surrounding(&mut con);
+
         let reply = warp::reply::html(
             hbs.render(
                 "article",
-                &json!(article)
+                &json!({
+                    "article": article,
+                    "prev": prev,
+                    "next": next
+                })
             ).expect("Failed to render article")
         );
 
@@ -260,6 +326,9 @@ fn create_handlebars() -> Handlebars<'static> {
     let mut hb = Handlebars::new();
     let index_tmpl_path = tmpl_path("index");
     let article_tmpl_path = tmpl_path("article");
+
+    hb.set_dev_mode(true);
+
     hb.register_template_file(
         "article",
         &article_tmpl_path
@@ -269,7 +338,6 @@ fn create_handlebars() -> Handlebars<'static> {
         &index_tmpl_path
     ).expect("Failed to register index template file");
     hb.register_helper("date_from_timestamp", Box::new(date_from_timestamp));
-    hb.register_helper("page_display", Box::new(page_display));
 
     hb
 }
@@ -288,7 +356,7 @@ async fn main() {
 
     let hbs2 = hbs.clone();
     let article_index = warp::path::end().map(move || {
-        index_at_offset(0, &hbs2)
+        index_at_offset(1, &hbs2)
     });
     let hbs3 = hbs.clone();
     let article_index_offset = warp::path!("page" / usize).map(move |page| {
@@ -302,12 +370,12 @@ async fn main() {
     let images = warp::path("images").and(warp::fs::dir("content/images"));
     let assets = warp::path("assets").and(warp::fs::dir("content/assets"));
 
-    let routes = article_index.
-        or(article_index_offset).
-        or(article).
-        or(images).
-        or(assets).
-        recover(file_not_found);
+    let routes = article_index
+        .or(article_index_offset)
+        .or(article)
+        .or(images)
+        .or(assets)
+        .recover(file_not_found);
 
     println!("Listening on 3090...");
     warp::serve(routes)
