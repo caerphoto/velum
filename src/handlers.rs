2881627600a6b374bff8a2d88aa16efcc967d705
Andy Farrell <git@andyf.me>
2022-10-10 15:40:34 +0100

    Re-implement index page routes

---
+++ src/handlers.rs
@@ -0,0 +1,36 @@
+pub mod index;
+
+use std::time::{
+    SystemTime,
+    UNIX_EPOCH,
+    Duration,
+};
+
+use axum::{
+    http::StatusCode,
+    response::Html,
+};
+
+const INTERNAL_SERVER_ERROR: u16 = 500;
+pub const BAD_REQUEST: u16 = 400;
+const ONE_YEAR: Duration = Duration::new(31_536_000, 0);
+
+
+
+fn create_timestamp() -> i64 {
+    match SystemTime::now().duration_since(UNIX_EPOCH) {
+        // i64 is enough milliseconds for 292 million years, so coercing it like
+        // this is probably fine.
+        Ok(d) => d.as_millis() as i64,
+        Err(e) => -(e.duration().as_millis() as i64)
+    }
+}
+
+pub fn server_error(msg: &str) -> (StatusCode, Html<String>) {
+    log::error!("{}", msg);
+    // Possible TODO: send HTML file
+    (
+        StatusCode::INTERNAL_SERVER_ERROR,
+        Html("Internal server error :(".into())
+    )
+}
---
+++ src/handlers/index.rs
@@ -0,0 +1,161 @@
+use std::time;
+use serde_json::json;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    extract::{Path, Extension},
+};
+use tower_cookies::Cookies;
+
+use crate::CommonData;
+use crate::article::storage::{
+    LinkList,
+    fetch_index_links,
+};
+pub use crate::SharedData;
+use super::server_error;
+
+// Integer division rounding up, for calculating page count
+fn div_ceil(lhs: usize, rhs: usize) -> usize {
+    let d = lhs / rhs;
+    let r = lhs % rhs;
+    if r > 0 && rhs > 0 {
+        d + 1
+    } else {
+        d
+    }
+}
+
+fn theme(cookies: Cookies) -> Option<String> {
+    cookies
+        .get("theme")
+        .and_then(|c| Some(c.value().to_string()))
+}
+
+fn render_article_list(
+    article_list: LinkList,
+    mut page: usize,
+    page_size: usize,
+    data: &CommonData,
+    tag: Option<&str>,
+    theme: Option<String>,
+) -> (StatusCode, Html<String>) {
+    let blog_title = &data.config.blog_title;
+    let last_page = div_ceil(article_list.total_articles, page_size);
+
+    let title = if let Some(tag) = tag {
+        String::from("Tag: ") + tag
+    } else {
+        String::from("Article Index")
+    };
+
+    // Page '0' is the home page: shows the same article list as the first index
+    // page, but has the additional home page info box.
+    let home_page_info = if page == 0 {
+        Some(&data.config.info_html)
+    } else {
+        None
+    };
+    page = std::cmp::max(page, 1);
+
+    match data.hbs.render(
+        "index",
+        &json!({
+            "blog_title": blog_title,
+            "title": title,
+            "prev_page": if page > 1 { page - 1 } else { 0 },
+            "current_page": page,
+            "next_page": if page < last_page { page + 1 } else { 0 },
+            "last_page": last_page,
+            "search_tag": tag.unwrap_or(""),
+            "article_count": article_list.total_articles,
+            "articles": &article_list.index_views,
+            "body_class": if tag.is_some() { "tag-index" } else { "index" },
+            "content_dir": &data.config.content_dir,
+            "theme": theme,
+            "home_page_info": home_page_info,
+        })
+    ) {
+        Ok(rendered_page) => {
+            (StatusCode::OK, Html(rendered_page))
+        },
+        Err(e) => {
+            server_error(
+                &format!(
+                    "Failed to render article in index. Error: {:?}", e
+                )
+            )
+        }
+    }
+}
+
+pub async fn home_handler(
+    Extension(data): Extension<SharedData>,
+    cookies: Cookies,
+)-> impl IntoResponse {
+    index_handler(Path(1), Extension(data), cookies).await
+}
+
+pub async fn index_handler(
+    Path(page): Path<usize>,
+    Extension(data): Extension<SharedData>,
+    cookies: Cookies,
+)-> impl IntoResponse {
+    let now = time::Instant::now();
+    let data = data.lock().unwrap();
+
+    let page_size = data.config.page_size;
+    let article_list = fetch_index_links(page, page_size, None, &data.articles);
+
+    let response = render_article_list(
+        article_list,
+        page,
+        page_size,
+        &data,
+        None,
+        theme(cookies),
+    );
+    log::info!(
+        "Rendered article index page {} in {}µs",
+        page,
+        now.elapsed().as_micros()
+    );
+
+    response.into_response()
+}
+
+pub async fn tag_home_handler(
+    Path(tag): Path<String>,
+    Extension(data): Extension<SharedData>,
+    cookies: Cookies,
+)-> impl IntoResponse {
+    tag_handler(Path((tag, 1)), Extension(data), cookies).await
+}
+
+pub async fn tag_handler(
+    Path((tag, page)): Path<(String, usize)>,
+    Extension(data): Extension<SharedData>,
+    cookies: Cookies,
+) -> impl IntoResponse {
+    let now = time::Instant::now();
+    let data = data.lock().unwrap();
+    let page_size = data.config.page_size;
+
+    let article_result = fetch_index_links(page, page_size, Some(&tag), &data.articles);
+
+    let response = render_article_list(
+        article_result,
+        page,
+        page_size,
+        &data,
+        Some(&tag),
+        theme(cookies),
+    );
+    log::info!(
+        "Rendered tag '{}' index page {} in {}µs",
+        &tag,
+        page,
+        now.elapsed().as_micros()
+    );
+    response
+}
