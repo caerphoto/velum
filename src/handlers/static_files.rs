88aa16dae7520128658a6f46f272a260f7273606
Andy Farrell <git@andyf.me>
2022-10-12 18:53:12 +0100

    Add separate JS files, and use manifest to serve them concatenated

--- src/handlers/static_files.rs
+++ src/handlers/static_files.rs
@@ -40,7 +40,7 @@ use super::{
 
 const ONE_YEAR: Duration = Duration::new(31_536_000, 0);
 
-fn read_file_bytes(filename: &PathBuf, buf: &mut Vec<u8>) -> IoResult<LastModified> {
+fn read_file_bytes<P: AsRef<FsPath>>(filename: P, buf: &mut Vec<u8>) -> IoResult<LastModified> {
     let mut f = fs::File::open(filename)?;
     let meta = f.metadata()?;
     let modified = meta.modified()?;
