c1d9924074835f56dd003bbefc6b4fb20a31dabd
Andy Farrell <github@andyf.me>
2022-10-15 20:31:06 +0100

    Feature/better theme selector (#41)
    * Dynamically generate theme list
    
    Theme list is built by searching for all .css files in content/assets/themes.
    The theme name is extracted from a comment on the first line of the CSS file.
    
    * Fix themes on articles
    
    * Inline style on 404 error page
    
    Added the ability to return HTML error pages from content/errors/<status code>.html

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,12 +1,13 @@
-pub mod index;
+pub mod admin;
 pub mod article;
 pub mod comment;
-pub mod admin;
+pub mod index;
 pub mod static_files;
 
-use std::time::{
-    SystemTime,
-    UNIX_EPOCH,
+use std::{
+    fs,
+    path::PathBuf,
+    time::{SystemTime, UNIX_EPOCH},
 };
 
 use axum::{
@@ -17,50 +18,58 @@ use tower_cookies::Cookies;
 
 pub type HtmlResponse = (StatusCode, Html<String>);
 
-pub fn theme(cookies: Cookies) -> Option<String> {
-    cookies
-        .get("theme")
-        .map(|c| c.value().to_string())
-}
-
 pub fn create_timestamp() -> i64 {
     match SystemTime::now().duration_since(UNIX_EPOCH) {
         // i64 is enough milliseconds for 292 million years, so coercing it like
         // this is probably fine.
         Ok(d) => d.as_millis() as i64,
-        Err(e) => -(e.duration().as_millis() as i64)
+        Err(e) => -(e.duration().as_millis() as i64),
     }
 }
 
-// TODO: render HTML from file
-pub fn render_error_page<T: std::fmt::Display>(status: StatusCode, additional_text: Option<T>) -> String {
-    if additional_text.is_some() {
-        format!("Error {:?}: {}", status, additional_text.unwrap())
-    } else {
-        format!("Error {:?}", status)
+pub fn theme(cookies: Cookies) -> Option<String> {
+    cookies.get("theme").map(|c| {
+        format!("themes/{}", c.value())
+    })
+}
 
+// TODO: render HTML from file
+pub fn render_error_page<T: std::fmt::Display>(
+    status: StatusCode,
+    additional_text: Option<T>,
+) -> String {
+    lazy_static! {
+        static ref ERRORS_DIR: PathBuf = {
+            let c = crate::config::Config::load().expect("Failed to load config");
+            PathBuf::from(c.content_dir).join("errors")
+        };
+    }
+    let message = if additional_text.is_some() {
+        format!("HTTP error {:?}: {}", status, additional_text.unwrap())
+    } else {
+        format!("HTTP error {:?}", status)
+    };
+    let filename = ERRORS_DIR.join(status.as_u16().to_string() + ".html");
+    if let Ok(content) = fs::read_to_string(&filename) {
+        content.replace("####", &message)
+    } else {
+        message
     }
 }
 
 pub fn server_error(msg: &str) -> HtmlResponse {
     log::error!("Server error: {}", msg);
-    (
-        StatusCode::INTERNAL_SERVER_ERROR,
-        Html(msg.to_string()),
-    )
+    (StatusCode::INTERNAL_SERVER_ERROR, Html(msg.to_string()))
 }
 
 pub fn empty_response(code: StatusCode) -> HtmlResponse {
-    (
-        code,
-        Html(String::new())
-    )
+    (code, Html(String::new()))
 }
 
 pub fn not_found(uri: Option<Uri>) -> HtmlResponse {
     (
         StatusCode::NOT_FOUND,
-        Html(render_error_page(StatusCode::NOT_FOUND, uri))
+        Html(render_error_page(StatusCode::NOT_FOUND, uri)),
     )
 }
 
