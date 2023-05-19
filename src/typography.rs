2d3ed956b18407520fa557817962e5e732a67706
Andy Farrell <git@andyf.me>
2023-05-19 17:41:30 +0100

    Allow closures in typography regex replacers

--- src/typography.rs
+++ src/typography.rs
@@ -1,47 +1,56 @@
-use regex::Regex;
+use regex::{Regex, Captures};
+
+type ReplacerFn = fn(&Captures) -> String;
 
 struct Typograph {
     r: Regex,
-    s: &'static str,
+    s: Option<&'static str>,
+    f: Option<ReplacerFn>,
 }
 
 pub fn typogrified(text: &str) -> String {
     lazy_static! {
         static ref REPLACEMENTS: Vec<Typograph> = vec![
-            Typograph { r: Regex::new("``").unwrap(), s: "“"},
-            Typograph { r: Regex::new("''").unwrap(), s: "”" },
+            Typograph { r: Regex::new("``").unwrap(), s: Some("“"), f: None },
+            Typograph { r: Regex::new("''").unwrap(), s: Some("”"), f: None  },
 
             // Decades, e.g. ’80s - may sometimes be wrong if it encounters a quote
             // that starts with a decade, e.g. '80s John Travolta was awesome.'
-            Typograph { r: Regex::new(r"['‘](\d\d)s").unwrap(),  s: "’$1s" },
+            Typograph { r: Regex::new(r"['‘](\d\d)s").unwrap(),  s: Some("’$1s"), f: None  },
 
             // Order of these is imporant – opening quotes need to be done first.
-            Typograph { r: Regex::new("`").unwrap(), s: "‘" },
-            Typograph { r: Regex::new(r#"(^|\s|\()""#).unwrap(), s: "$1“" }, // ldquo
-            Typograph { r: Regex::new(r#"""#).unwrap(),          s: "”" },   // rdquo
+            Typograph { r: Regex::new("`").unwrap(), s: Some("‘"), f: None  },
+            Typograph { r: Regex::new(r#"(^|\s|\()""#).unwrap(), s: Some("$1“"), f: None  }, // ldquo
+            Typograph { r: Regex::new(r#"""#).unwrap(),          s: Some("”"), f: None  },   // rdquo
 
-            Typograph { r: Regex::new(r"(^|\s|\()'").unwrap(),   s: "$1‘" }, // lsquo
-            Typograph { r: Regex::new("'").unwrap(),             s: "’" },   // rsquo
+            Typograph { r: Regex::new(r"(^|\s|\()'").unwrap(),   s: Some("$1‘"), f: None  }, // lsquo
+            Typograph { r: Regex::new("'").unwrap(),             s: Some("’"), f: None  },   // rsquo
 
             // Dashes
             // \u2009 = thin space
             // \u200a = hair space
             // \u2013 = en dash
             // \u2014 = em dash
-            Typograph { r: Regex::new(r"\b–\b").unwrap(),   s: "\u{200a}\u{2013}\u{200a}" },
-            Typograph { r: Regex::new(r"\b—\b").unwrap(),   s: "\u{200a}\u{2014}\u{200a}" },
-            Typograph { r: Regex::new(" — ").unwrap(),      s: "\u{200a}\u{2014}\u{200a}" },
-            Typograph { r: Regex::new("---").unwrap(),      s: "\u{200a}\u{2014}\u{200a}" },
-            Typograph { r: Regex::new(" - | -- ").unwrap(), s: "\u{2009}\u{2013}\u{2009}" },
-            Typograph { r: Regex::new("--").unwrap(),       s: "\u{200a}\u{2013}\u{200a}" },
+            Typograph { r: Regex::new(r"\b–\b").unwrap(),   s: Some("\u{200a}\u{2013}\u{200a}"), f: None  },
+            Typograph { r: Regex::new(r"\b—\b").unwrap(),   s: Some("\u{200a}\u{2014}\u{200a}"), f: None  },
+            Typograph { r: Regex::new(" — ").unwrap(),      s: Some("\u{200a}\u{2014}\u{200a}"), f: None  },
+            Typograph { r: Regex::new("---").unwrap(),      s: Some("\u{200a}\u{2014}\u{200a}"), f: None  },
+            Typograph { r: Regex::new(" - | -- ").unwrap(), s: Some("\u{2009}\u{2013}\u{2009}"), f: None  },
+            Typograph { r: Regex::new("--").unwrap(),       s: Some("\u{200a}\u{2013}\u{200a}"), f: None  },
 
-            Typograph { r: Regex::new(r"\.\.\.").unwrap(), s: "…" } // hellip
+            Typograph { r: Regex::new(r"\.\.\.").unwrap(), s: Some("…"), f: None }, // hellip
+
+            Typograph { r: Regex::new(r"\b(\d+)x").unwrap(), s: None, f: Some(|caps| String::from(caps.get(1).unwrap().as_str()) + "×") }
         ];
     }
 
     let mut new_text = String::from(text);
     for typograph in REPLACEMENTS.iter() {
-        new_text = typograph.r.replace_all(&new_text, typograph.s).into_owned();
+        if typograph.f.is_some() {
+            new_text = typograph.r.replace_all(&new_text, typograph.f.unwrap()).into_owned();
+        } else {
+            new_text = typograph.r.replace_all(&new_text, typograph.s.unwrap()).into_owned();
+        }
     }
 
     new_text
